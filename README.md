d23a52500a7ea163574fffe7c94cf9487787643c
Di Lu <lud2@blender06.cs.rpi.edu>
2019-04-27 14:50:55 -0400

    create

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Geonames-Gazetteer-Extraction
+The script is a tool to extrac gazetteer for target language from the database released by [Geonames](https://www.geonames.org/).
+Th script is written in Pythton 3.6+
+# Usage
+[Downlad](https://www.dropbox.com/s/oweqmcdwesa701j/alternateNames.txt?dl=0) alternateNames.txt file
+
+[Downlad](https://www.dropbox.com/s/jik9lvx13wp8zhd/allCountries.txt?dl=0) allCountries.txt
+
+'''
+python gaz_extraction.py  --target_language LAN_CODE --output_directory output_directory OUTPUT_DIR --path_allCountries FILE_PATH_1 --path_alternateNames FILE_PATH_2
+'''
+LAN_CODE is ISO 639-1 language code
+
+output format
+```
+圣胡利娅-德洛里亚       Sant Julià de Lòria
+圣胡利娅－德洛里亚      Sant Julià de Lòria
+奥尔迪诺        Ordino
+萊塞斯卡爾德－恩戈爾達  les Escaldes
+恩坎普  Encamp
+卡尼略  Canillo
+卡尼略  Canillo
+````
---
+++ gaz_extraction.py
@@ -0,0 +1,60 @@
+#-*- coding:utf-8 -*-
+import os
+import sys
+import argparse
+#countryList=[]
+#fCountry=open('/data/m1/lud2/LORELEI/resource/geonames/countryInfo.txt')
+#for oneline in fCountry:
+#    if oneline[0:1]=='#':
+#        continue
+#    if oneline.split('\t')[8]=='AF':
+#        countryList.append(oneline.split('\t')[0])
+#fCountry.close()
+#print len(countryList)
+
+
+def il_geonames(lang,falna,dir_out,fGeonames):
+    cadList={}
+    with open(falna,'r') as f:
+        for oneline in f:
+            if oneline.strip().split('\t')[2]==lang:
+                if oneline.strip().split('\t')[1] in cadList:
+                    cadList[oneline.strip().split('\t')[1]].append(oneline.strip().split('\t')[3])
+                else:
+                    cadList[oneline.strip().split('\t')[1]]=[oneline.strip().split('\t')[3]]
+
+
+
+
+
+
+    f_out=open(os.path.join(dir_out,'%s_geonames.txt'%lang),'w')
+
+    geoDic={}
+    n=0
+
+    with open(fGeonames,'r') as f:
+        for oneline in f:
+            n+=1
+            geonameid = oneline.split('\t')[0]
+            name= oneline.split('\t')[1]
+            asciiname = oneline.split('\t')[2]
+            alternatenames = oneline.split('\t')[3]
+            if geonameid in cadList:
+                for onealt in cadList[geonameid]:
+                    f_out.write('%s\t%s\n'%(onealt, name))
+    f_out.close()
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Extracting gazetteer from Geonames')
+    parser.add_argument('--target_language', default='zh', required = True, help='the 2-digit language code for target language')
+    parser.add_argument('--output_directory', default='data/gaz/', help='the path of the folder to save the extracted lexicon')
+    parser.add_argument('--path_allCountries', default='data/allCountries.txt', help='path of allCountries.txt')
+    parser.add_argument('--path_alternateNames', default='data/alternateNames.txt', help='path of alternateNames.txt')
+    args = parser.parse_args()
+    #falna='/data/m1/lud2/LORELEI/resource/geonames/alternateNames.txt'
+    #fGeonames='/data/m1/lud2/LORELEI/resource/geonames/allCountries.txt'
+    #dir_out='../../geonames'
+    #    lang='am'
+
+    il_geonames(args.target_language,args.path_alternateNames,args.output_directory,args.path_allCountries)
